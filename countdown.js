21ed467f712aa0fdbaef304630c37454d90191dd
dguet <dguet@student.42angouleme.fr>
2023-01-13 14:42:38 +0100

    feat: done

---
+++ countdown.js
@@ -0,0 +1,16 @@
+function gettime() {
+  var date = new Date();
+  var time = date.getTime() / 1000;
+  time = 1673996400 - Math.floor(time);
+  return time;
+}
+
+var interval = setInterval(function () {
+  var count = gettime();
+  var hours = Math.floor(count / 3600);
+  var minutes = Math.floor((count - hours * 3600) / 60);
+  var seconds = Math.floor(count - hours * 3600 - minutes * 60);
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
